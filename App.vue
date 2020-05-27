fad3e41a771fc6f6b31c18588fc70d0a935ebd05
Stasen-star <65992259+Stasen-star@users.noreply.github.com>
2020-05-27 05:55:56 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link  to="/">
+          <v-list-item-action>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Сотрудники</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/operators">
+          <v-list-item-action>
+            <v-icon>mdi-cellphone</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title >Операторы</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>База Данных Телефонов</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <v-container>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col>
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
+</script>
